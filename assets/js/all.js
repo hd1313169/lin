1fc021d8ec541a3611b3e6bfc14b03efbaeb8218
Ryan Chiang <wsad71155@gmail.com>
2023-06-26 00:28:10 +0800

    Update 2023-06-25T16:28:07.916Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,12 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 0,
+  centeredSlides: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
 });
 //# sourceMappingURL=all.js.map
